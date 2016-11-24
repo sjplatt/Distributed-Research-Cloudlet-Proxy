3f0f761bf31e2f74ec7d112ac9994c2c3bc098c5
Sam Platt <samuel.j.platt@gmail.com>
2016-11-24 17:50:55 -0500

    Remove get cache method (only callback)

--- mysite/proxy/urls.py
+++ mysite/proxy/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^getCache/$', views.getCache, name='getCache'),
     url(r'^callBack/$', views.callBack, name='callBack'),
 ]
+
+# url(r'^getCache/$', views.getCache, name='getCache'),
--- mysite/proxy/views.py
+++ mysite/proxy/views.py
@@ -11,35 +11,6 @@ ni.ifaddresses('eth0')
 
 cache = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-# Helper methods
-
-def customDeserializeDatetime(obj):
-    try:
-        if "datetime" in str(obj):
-            return dateutil.parser.parse(obj.split(":")[1])
-        else:
-            return obj
-    except:
-        return obj
-
-def customSerializeDatetime(obj):
-    if isinstance(obj, datetime.datetime):
-        return "datetime:" + obj.isoformat()
-    raise TypeError("Type not serializable Boo")
-
-def convertToValue(data, rc, error, ret):
-    return json.dumps([ret, data, rc, error], default=customSerializeDatetime)
-
-def removeDateTimeFromKey(key):
-    if not "datetime" in str(key):
-        return key
-
-    body = key.split("|")
-    query, params = body[0], None
-    if len(body) == 2:
-        params = json.loads(body[1], object_pairs_hook=customDeserializeDatetime)
-        params = [p for p in params if not "datetime" in str(p)]
-
 # Returns the datetime parameter from params
 def removeDateTime(params):
     if params is None:
@@ -51,95 +22,6 @@ def removeDateTime(params):
                 res += [temp]
 
         return tuple(res)
-
-def convertToKey(sql, params):
-    if params is None:
-        return str(sql)
-    elif not "datetime" in str(params):
-        return str(sql) + "|" + str(params)
-    else:
-        return str(sql) + "|" + json.dumps(params, default=customSerializeDatetime)
-
-def putInCache(sql, params, data, rc, error):
-    params = removeDateTime(params)
-
-    if not "SELECT" in str(sql):
-        return
-
-    if params is None:
-        key = str(sql)
-        cache.set(key, json.dumps([data, rc, str(error)], default=customSerializeDatetime))
-    else:
-        key = str(sql) + "|" + str(params)
-        cache.set(key, json.dumps([data, rc, str(error)], default=customSerializeDatetime))
-
-def sendQueryToCloud(sql, params):
-    params = json.dumps(params, default=customSerializeDatetime)
-    key = str(sql) + "|" + params
-    ip = ni.ifaddresses('eth0')[2][0]['addr']
-    # Send the request to the cloud and get the response
-    r = requests.post("http://54.213.170.131:8000/polls/", data=key, headers={"source":ip})
-    ret, data, rc, error = json.loads(r.text)
-    
-    data = [customDeserializeDatetime(d) for d in data]
-
-    return ret, data, rc, error
-
-def lookupCache(sql, params):
-    params = removeDateTime(params)
-    if params is None:
-        value = cache.get(str(sql))
-        if value:
-            values = json.loads(value)
-            
-            mydata = [customDeserializeDatetime(v) for v in values[0]]
-            rc = values[1]
-            error = True
-            if values[2] == "False":
-                error = False
-            
-            return mydata, rc, error, None
-        else:
-            return False
-    else:
-        value = cache.get(str(sql) + "|" + str(params))
-        if value:
-            values = json.loads(value)
-            
-            mydata = [customDeserializeDatetime(v) for v in values[0]]
-            rc = values[1]
-            error = True
-            if values[2] == "False":
-                error = False
-
-            return mydata, rc, error, None
-        else:
-            return False
-
-@csrf_exempt
-def getCache(request):
-    body = request.body.split("|")
-    sql, params = body[0], None
-
-    if len(body) == 2:
-        params = json.loads(body[1], object_pairs_hook=customDeserializeDatetime)
-        if not params is None:
-            params = [customDeserializeDatetime(p) for p in params]
-
-    # If the element is in the cache return None
-    if lookupCache(sql, params):
-        # print("HIT")
-        val, rc, error, ret = lookupCache(sql, params)
-        return HttpResponse(convertToValue(val, rc, error, ret))
-    else:
-        print("MISS")
-        ret, val, rc, error = sendQueryToCloud(sql,params)
-
-        # Only add to the cache if ret is not null
-        if ret == None:
-            putInCache(sql, params, val, rc, error)
-        
-        return HttpResponse(convertToValue(val, rc, error, ret))
 
 @csrf_exempt
 def callBack(request):
